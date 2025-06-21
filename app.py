41ec914b1ec2864dfafbe8992551516ec71034d8
sandeep9399 <sandeep.changlani@gmail.com>
2025-06-21 23:47:27 +0530

    app.py

--- app.py
+++ app.py
@@ -171,8 +171,6 @@ ph_tf = placeholder_box.text_frame
 ph_tf.text = f'''[AI image placeholder]
 Prompt:
 {row['Prompt']}'''
-Prompt:
-{row['Prompt']}'''
 
             if split_required:
                 slide2 = new_ppt.slides.add_slide(layout)
